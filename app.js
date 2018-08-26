ac3fa82209c4bd8c0ba556d537be02442eada8be
cipherswan <leskauskas25@gmail.com>
2018-08-26 16:41:39 +0300

    POST method to fetch data from last.fm API

--- app.js
+++ app.js
@@ -3,18 +3,31 @@ var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
 var port = 3000;
+var artistObj;
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-var artistObj;
 
 app.engine('html', require('ejs').renderFile);    
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
     res.render('index.html');
+});
+
+app.post('/post', function (req, res) {
+    request.get("http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + req.body.artistName + "&api_key=" + process.env.API + "&format=json", (error, response, body) => {
+        if(error) {
+            return console.dir(error);
+        }
+        artistObj = JSON.parse(body);
+        console.log(artistObj);
+        res.render('artist.html',{
+            artist: artistObj
+        });
+    });
 });
 
 app.listen(process.env.PORT || port, ()=> console.log("Server is running on port " + port));
